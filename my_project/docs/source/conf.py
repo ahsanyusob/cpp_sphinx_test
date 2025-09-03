def7ded11243e12c2794aa3768592a09459b1aba
Ahsan Yusob <ahm6464m@hs-coburg.de>
2025-09-03 10:24:33 +0200

    modified conf.py and index.rst; generated new index.html with `make html` in docs

--- my_project/docs/source/conf.py
+++ my_project/docs/source/conf.py
@@ -13,7 +13,13 @@ author = 'Ahsan Yusob'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["breathe"]
+
+# Path to Doxygen XML
+breathe_projects = {
+    "MyProject": "../build/xml"
+}
+breathe_default_project = "MyProject"
 
 templates_path = ['_templates']
 exclude_patterns = []
